c7f1fd7cbb26e077e9c7a40100b6ad6c115166f1
Joiman <joiman@gmail.com>
2018-08-31 20:07:03 -0500

    first commit

--- src/app/result/result.component.ts
+++ src/app/result/result.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 })
 export class ResultComponent implements OnInit {
   @Input() likes: number;
-  @Output() darleClick = new EventEmitter<boolean>();
+  //@Output() darleClick = new EventEmitter<boolean>();
   constructor() { }
 
   ngOnInit() {
